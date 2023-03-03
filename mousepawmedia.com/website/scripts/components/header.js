f2850c680107ed2b07b4288055130737e6f086d1
jonathancdev <jonathancdev@protonmail.com>
2023-03-03 18:27:26 +0100

    refactor: move html files to root and update paths

--- mousepawmedia.com/website/scripts/components/header.js
+++ mousepawmedia.com/website/scripts/components/header.js
@@ -6,33 +6,33 @@ class HeaderComponent extends HTMLElement {
         <!-- mobile logo -->
         <div class="logo mobile">
           <a href="./index.html">
-            <img src="../img/logo/mousepawmedia_logo.png"/>
+            <img src="/img/logo/mousepawmedia_logo.png"/>
           </a>
         </div>
         <!-- wordmark logo -->
         <div class="logo">
           <a href="./index.html">
-            <img src="../img/logo/mousepawmedia_wordmark_logo.png"/>
+            <img src="/img/logo/mousepawmedia_wordmark_logo.png"/>
           </a>
         </div>
         <!-- mobile menu toggle icon -->
         <button class="toggle-menu-button" id="toggle-menu-button">
-          <img id="hamburger-icon" class="visible" src="../img/icons/svg/hamburger.svg" />
-          <img id="close-icon" src="../img/icons/svg/close.svg" />
+          <img id="hamburger-icon" class="visible" src="/img/icons/svg/hamburger.svg" />
+          <img id="close-icon" src="/img/icons/svg/close.svg" />
         </button>
         <!-- standard nav -->
         <nav class="menu">
-            <a href="./company.html">Company</a>
-            <a href="./projects.html">Projects</a>
-            <a href="./developers.html">Developers</a>
+            <a href="/company.html">Company</a>
+            <a href="/projects.html">Projects</a>
+            <a href="/developers.html">Developers</a>
             <div>
               <p>Internship</p>
-              <img src="../../img/icons/svg/caret-down.svg"/>
+              <img src="/img/icons/svg/caret-down.svg"/>
               <!-- standard submenu -->
               <nav class="submenu">
-                <a href="./internship.html">Internship</a>
-                <a href="./mentorship.html">Mentorship</a>
-                <a href="./recruiting.html">Recruiting</a>
+                <a href="/internship.html">Internship</a>
+                <a href="/mentorship.html">Mentorship</a>
+                <a href="/recruiting.html">Recruiting</a>
             </nav>
             </div>
         </nav>
--- mousepawmedia.com/website/scripts/components/menu.js
+++ mousepawmedia.com/website/scripts/components/menu.js
@@ -4,17 +4,17 @@ class MenuComponent extends HTMLElement {
     this.innerHTML = /*html*/ `
         <!-- mobile menu and nav -->
         <nav class="menu" id="menu">
-            <a href="./company.html">Company</a>
-            <a href="./projects.html">Projects</a>
-            <a href="./developers.html">Developers</a>
+            <a href="/company.html">Company</a>
+            <a href="/projects.html">Projects</a>
+            <a href="/developers.html">Developers</a>
             <button id="toggle-submenu-button">
-              <span>Internships<img id="submenu-caret-icon" src="../../img/icons/svg/caret-down.svg"/></span>
+              <span>Internships<img id="submenu-caret-icon" src="/img/icons/svg/caret-down.svg"/></span>
 
             </button>
             <nav class="submenu" id="submenu">
-              <a href="./internship.html">Internship</a>
-              <a href="./mentorship.html">Mentorship</a>
-              <a href="./recruiting.html">Recruiting</a>
+              <a href="/internship.html">Internship</a>
+              <a href="/mentorship.html">Mentorship</a>
+              <a href="/recruiting.html">Recruiting</a>
             </nav>
         </nav>
 `;
